0fb76cb68fda1600827ecbf9a80d791fa03eb89f
Stuart <stuart@smbkr.xyz>
2021-07-20 10:09:24 +0100

    Cleanup unused dependencies and files

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,8 +3,12 @@ import handler from './index'
 
 describe('handler', () => {
   it('returns status 200', async () => {
-    const promise = handler({} as APIGatewayProxyEvent, undefined, undefined)
+    const response = await handler(
+      {} as APIGatewayProxyEvent,
+      undefined,
+      undefined
+    )
 
-    await expect(promise).resolves.toMatchObject({ statusCode: 200 })
+    expect(response).toMatchObject({ statusCode: 200 })
   })
 })
